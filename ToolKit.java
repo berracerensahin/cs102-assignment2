8d7d777165aecd43114d285dd2c5d8732fcfaac0
Bensu Eseryel <bensu.eseryel@ug.bilkent.edu.tr>
2026-01-28 14:49:01 +0300

    creating the array

--- ToolKit.java
+++ ToolKit.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+public class ToolKit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
+        for(int i=0; i<size; i++){
+            numbers[i]=(int)(Math.random()*101);
+        }
+    }
+    
+}
