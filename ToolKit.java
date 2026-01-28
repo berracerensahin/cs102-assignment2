c8320a5ebb82b82a0ea3d616d49a274ec2728ec7
mina <minaturkyasar@gmail.com>
2026-01-28 14:50:52 +0300

    Update ToolKit.java

--- ToolKit.java
+++ ToolKit.java
@@ -10,3 +10,23 @@ public class ToolKit {
     }
     
 }
+public static int maxValue (int[] arr) {
+    int max = arr[0];
+    for (int i = 0; i < arr.length(); i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+public static int minValue (int[] arr) {
+    int min = arr[0];
+    for (int i = 0; i < arr.length(); i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
